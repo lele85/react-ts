03275e67f0ec0aaf87b1e1fc19ad2aba60d8c4aa
Emanuele Rampichini <emanuele.rampichini@gmail.com>
2018-02-20 09:44:44 +0100

    Imported variables directly from component scss module

--- src/components/TestStyle.tsx
+++ src/components/TestStyle.tsx
@@ -1,17 +1,16 @@
 import React from 'react';
-import { StatelessComponent } from "react";
+import { StatelessComponent } from 'react';
 import styles from './TestStyle.scss';
-import colors from '../styles/colors.scss';
 
 export const TestStyle: StatelessComponent<{}> = () => {
     return (
         <div className={styles.componentBorder}>
             <h1>Colors from SCSS variables:</h1>
             <ul>
-               <li style={{backgroundColor:colors.accentColor, color:"white"}}>{colors.accentColor}</li>
-               <li style={{backgroundColor:colors.errorColor, color:"white"}}>{colors.errorColor}</li> 
-               <li style={{backgroundColor:colors.warningColor, color:"white"}}>{colors.warningColor}</li> 
-               <li style={{backgroundColor:colors.infoColor, color:"white"}}>{colors.infoColor}</li>  
+               <li style={{backgroundColor:styles.accentColor, color:"white"}}>{styles.accentColor}</li>
+               <li style={{backgroundColor:styles.errorColor, color:"white"}}>{styles.errorColor}</li> 
+               <li style={{backgroundColor:styles.warningColor, color:"white"}}>{styles.warningColor}</li> 
+               <li style={{backgroundColor:styles.infoColor, color:"white"}}>{styles.infoColor}</li>  
             </ul>
         </div>
         
